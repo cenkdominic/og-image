1b8cbac15073242e14e3a6103ac7e90a2a3487d6
Cenk <cenkdominic@gmail.com>
2022-02-21 17:22:54 +0000

    Author string ! test

--- api/_lib/parser.ts
+++ api/_lib/parser.ts
@@ -33,7 +33,7 @@ export function parseRequest(req: IncomingMessage) {
     const parsedRequest: ParsedRequest = {
         fileType: extension === 'jpeg' ? extension : 'png',
         text: decodeURIComponent(text),
-        author: author,
+        author: author!,
         theme: theme === 'dark' ? 'dark' : 'light',
         md: md === '1' || md === 'true',
         fontSize: fontSize || '96px',
